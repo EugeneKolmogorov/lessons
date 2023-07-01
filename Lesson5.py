852f4fcf399c46c470aab662daedf20b69fe11e7
EugeneKolmogorov <136169388+EugeneKolmogorov@users.noreply.github.com>
2023-07-02 00:38:28 +1000

    Update Lesson5.py

--- Lesson5.py
+++ Lesson5.py
@@ -1,23 +1,22 @@
 # Задача № 26
 
+# def f(a, b):
+#     if b == 1:
+#         return a
+#     if b != 1:
+#         return a * f(a, b-1)
 # a = int(input())
 # b = int(input())
-# x = 1
-# while b != 0:
-#     x = x * a
-#     b -= 1
-# print(x)
+# print(f(a, b))
 
 # Задача № 28
 
-def summa():
-    a = int(input())
-    b = int(input())
-
-    while b!= 0:
-        a += 1
-        b -= 1
-
-    print(a)
-
-summa()
+def sum(a, b):
+    
+    if b == 0:
+        return a
+    if b != 0:
+        return sum(a+1, b-1)
+a = int(input())
+b = int(input())
+print(sum(a, b))
