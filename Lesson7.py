55e831755077b5dfac18757d56e90fffe2dc9c38
EugeneKolmogorov <136169388+EugeneKolmogorov@users.noreply.github.com>
2023-07-06 19:38:32 +1000

    Add files via upload

---
+++ Lesson7.py
@@ -0,0 +1,23 @@
+# Задача № 34
+# line = input().lower()
+# lines = line.split()
+
+
+# lst = [sum(x in 'уеыаоэяию' for x in lin)
+#         for lin in lines]
+
+# if len(set(lst)) == 1:
+#     res = "Парам пам-пам"
+# else:
+#     res = "Пам парам"
+
+# print(res)
+
+# Задача № 36
+
+def print_operation_table(operation, num_rows = 6, num_columns = 6):
+    a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows +1)]
+    for i in a:
+        print(*[f'{x:3}' for x in i])
+
+print_operation_table(lambda x, y: x*y)
